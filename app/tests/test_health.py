8e505ab1f534b16ac40b2c27ffe6a1cd2f0d0fc3
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-04-02 19:16:32 -0500

    simplify & clean up

--- app/tests/test_health.py
+++ app/tests/test_health.py
@@ -1,18 +1,18 @@
 from xpresso.testclient import TestClient
 
-from app.main import app
+from app.main import create_app
 from app.db import ConnectionHealth
 
 
 def test_health() -> None:
-    class FakeHealth(ConnectionHealth):
-        def __init__(self) -> None:
-            pass
+    class DBHealthStub:
         async def is_connected(self) -> bool:
             return True
 
+    app = create_app(DBHealthStub())
+
     with app.dependency_overrides as overrides:
-        overrides[ConnectionHealth] = FakeHealth
+        overrides[ConnectionHealth] = DBHealthStub
 
         with TestClient(app) as client:
             resp = client.get("/health")
